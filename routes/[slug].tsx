f9f6f621defba632718c86c65385f2efd60aca00
d20502-d <s4000050@student.rmit.edu.au>
2024-08-07 12:37:52 +1000

    c

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -23,8 +23,8 @@ export default function PostPage(props: PageProps<Post>) {
         <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
       </Head>
       <h2>
-      <a class="text-gray-800">&nbsp;|&nbsp;</a> 
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post">001</a><a class="text-gray-800">&nbsp;|</a>  
+      <a class="text-gray-100">&nbsp;|&nbsp;</a> 
+        <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post">001</a><a class="text-gray-500">&nbsp;|</a>  
         <a href="https://d20502-d-dms1-blog-38.deno.dev/second-blog-post"> 002</a>  |
         <a href="https://d20502-d-dms1-blog-38.deno.dev/third-blog-post"> 003</a>  |
         <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post"> 004</a>  |
