88f2db939c5f0c185171497b6b840190ac5097c8
d20502-d <s4000050@student.rmit.edu.au>
2024-08-07 13:45:29 +1000

    1

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -50,6 +50,7 @@ export default function PostPage(props: PageProps<Post>) {
     />
 
   </main>
+  <br></br>
   <h2>
   <a class="text-gray-300">&nbsp;|&nbsp;</a> 
     <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post" class="text-gray-600">001</a><a class="text-gray-300">&nbsp;|&nbsp;</a>  
@@ -58,6 +59,7 @@ export default function PostPage(props: PageProps<Post>) {
     <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post" class="text-gray-600">004</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
     <a href="https://d20502-d-dms1-blog-38.deno.dev/fifth-blog-post" class="text-gray-600">005</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
   </h2>
+  <br></br>
   </font> 
     </>
   );
