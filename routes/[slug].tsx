eedd6c2d9c990ca9ce4d9ac42aa28ddaca6d4160
d20502-d <s4000050@student.rmit.edu.au>
2024-08-07 12:52:04 +1000

    f

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -17,47 +17,47 @@ export const handler: Handlers<Post> = {
 export default function PostPage(props: PageProps<Post>) {
   const post = props.data;
   return (
-    <>
-      <Head>
-        <style dangerouslySetInnerHTML={{ __html: CSS }} />
-        <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
-      </Head>
-      <h2>
-      <a class="text-gray-300">&nbsp;|&nbsp;</a> 
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post" class="text-gray-600">001</a><a class="text-gray-300">&nbsp;|&nbsp;</a>  
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/second-blog-post" class="text-gray-700">002</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/third-blog-post" class="text-gray-600">003</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post" class="text-gray-600">004</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/fifth-blog-post" class="text-gray-600">005</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
-      </h2>
-      <main class="max-w-screen-md px-4 pt-16{padding-top: 2rem;} mx-auto">
-        <h1 class="text-5xl font-bold">{post.title}</h1>
-        <time class="text-gray-500">
-          {new Date(post.publishedAt).toLocaleDateString("en-us", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          })}
-        </time>
+    <><font face="Times New Roman"><Head>
+    <style dangerouslySetInnerHTML={{ __html: CSS }} />
+    <style dangerouslySetInnerHTML={{ __html: KATEX_CSS }} />
+  </Head>
+  <h2>
+  <a class="text-gray-300">&nbsp;|&nbsp;</a> 
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post" class="text-gray-600">001</a><a class="text-gray-300">&nbsp;|&nbsp;</a>  
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/second-blog-post" class="text-gray-600">002</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/third-blog-post" class="text-gray-600">003</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post" class="text-gray-600">004</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/fifth-blog-post" class="text-gray-600">005</a><a class="text-gray-300">&nbsp;|&nbsp;</a> 
+  </h2>
+  <main class="max-w-screen-md px-4 pt-16{padding-top: 2rem;} mx-auto">
+    <h1 class="text-5xl font-bold">{post.title}</h1>
+    <time class="text-gray-500">
+      {new Date(post.publishedAt).toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })}
+    </time>
 
 
 
-        <div
-          class="mt-8 markdown-body"
-          dangerouslySetInnerHTML={{ __html: render(post.content, {
-            disableHtmlSanitization: post.disableHtmlSanitization,
-            allowMath: post.allowMath,
-          }) }}
-        />
+    <div
+      class="mt-8 markdown-body"
+      dangerouslySetInnerHTML={{ __html: render(post.content, {
+        disableHtmlSanitization: post.disableHtmlSanitization,
+        allowMath: post.allowMath,
+      }) }}
+    />
 
-      <br>          |
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post">  001</a>  |
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/second-blog-post">  002</a>  |
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/third-blog-post"> 003</a>  |
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post"> 004</a>  |
-        <a href="https://d20502-d-dms1-blog-38.deno.dev/fifth-blog-post"> 005</a>  |</br>
+  <br>          |
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/first-blog-post">  001</a>  |
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/second-blog-post">  002</a>  |
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/third-blog-post"> 003</a>  |
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/fourth-blog-post"> 004</a>  |
+    <a href="https://d20502-d-dms1-blog-38.deno.dev/fifth-blog-post"> 005</a>  |</br>
 <br></br>
-      </main>
+  </main>
+  </font> 
     </>
   );
 }
