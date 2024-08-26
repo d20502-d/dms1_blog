cb99b848fcc6e9a20d22be17c3ad4a45ca3baa3a
d20502-d <s4000050@student.rmit.edu.au>
2024-08-27 09:48:18 +1000

    1

--- routes/index.tsx
+++ routes/index.tsx
@@ -24,8 +24,8 @@ function PostCard(props: { post: Post }) {
   const { post } = props;
   return (
     <div class="py-8 border(t gray-200)">
-      <a class="sm:col-span-2" href="https://d20502-d-dms1-blog-38.deno.dev/content">
-        <h3 class="text(3xl gray-900) font-bold" href={`/${post.slug}`}>
+      <a class="sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">
           {post.title}
         </h3>
         <time class="text-gray-500">
